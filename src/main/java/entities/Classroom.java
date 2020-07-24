dc36695965a37dad00ec391f7f6fe8188531cb22
Leonardo Daza <leonardo.daza@truextend.com>
2020-07-24 19:11:36 -0400

    [develop] Created Classroom entity.

---
+++ src/main/java/entities/Classroom.java
@@ -0,0 +1,18 @@
+package entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Classroom {
+
+    private String name;
+    private double latitude;
+    private double longitude;
+
+}
